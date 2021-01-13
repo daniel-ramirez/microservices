0f647d349a7bc52c7603ff52bdd27e40b219e741
Daniel Ramirez <danielfr1987@gmail.com>
2021-01-12 21:57:15 -0600

    Service registry init project

---
+++ spring-boot/service-registry/src/main/java/com/darf/service/ServiceRegistryApplication.java
@@ -0,0 +1,13 @@
+package com.darf.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceRegistryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceRegistryApplication.class, args);
+	}
+
+}
